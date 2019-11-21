82f30826f65e1c24318535c3c08fdf7981ac891c
Luís Margalho <luismargalho@boldint.com>
2019-11-21 08:16:44 +0000

    init commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id " v-for="todo in todos">
+            <TodosItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodosItem from "./TodosItem.vue";
+export default {
+    name: "Todos",
+    components: {
+        TodosItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
